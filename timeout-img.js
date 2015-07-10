495135bb6b201ae6ca91d4e55ec4090944960228
syuzyuzyu <syuzyuzyu@gmail.com>
2015-07-10 19:27:43 +0900

    make change

--- timeout-img.js
+++ timeout-img.js
@@ -1,9 +1,10 @@
-(function(){
+(function(window, document){
   'use strict';
-  var Redirect = Redirect || {};
-  Redirect = function(ele){
+  //var Redirect = Redirect || {};
+  var Redirect = function(ele){
    this._ele = ele;
-   this._interval = 3000;
+   this.src = ele.src;
+   this._interval = 1;
    this._ele.onload = function () {
             //TODO event cancel
     };
@@ -11,21 +12,6 @@
             //TODO event cancel
 
     };
-    /*function timeout(){
-        window.setTimeout(function() {
-          obj.parentNode.removeChild(element) || obj.src = '';
-          if(window.stop !== undefined)
-          {
-               window.stop();//to do?
-          }
-        }, this._interval);
-    }*/
-    function ridirect(){
-
-    }
-
-    /*this._ele.src = this._ele.getAttribute('data-original');
-    timeout();*/
   }
   Redirect.prototype = Object.create(null, {
     constructor: {
@@ -34,17 +20,22 @@
     timeout: {
       value: function() {
         console.log(this._ele.clientHeight);
-        var obj = this._ele;
+        var obj = this._ele, src = this.src;
+        console.log(src);
         window.setTimeout(function() {
-            if(obj.parentNode === undefined){obj.src = '';}
-            //obj.parentNode.removeChild(element) || obj.src = '';
+            console.log(obj.parentNode);
+            if(obj.parentNode !== undefined){obj.src = '';}
+            obj.parentNode.removeChild(obj);// || obj.src = '';
+            callback();
             if(window.stop !== undefined) {
                  window.stop();//to do?
             }
         }, this._interval);
-        var modosu = function(){}
-        callback = function(){
-            obj.parentNode.removeChild(element);
+        var callback = function(){
+          var change_img = document.createElement('img');
+          change_img.setAttribute('src', src);
+          obj.appendChild(change_img);
+          //obj.src(this.);
         }
       }
     }
@@ -55,28 +46,4 @@
       new Redirect(img_tag[i]).timeout();
     }
   }
-})();
-
-
-
-/**
-Redirect.prototype = Object.create(null, {
-  constructor: {
-    value: Redirect
-  },
-  timeout: {
-    value: function() {
-      console.log(this._ele.clientHeight);
-      var obj = this._ele;
-      window.setTimeout(function() {
-          callback();
-      }, this._interval);
-
-      callback = function(){
-          obj.parentNode.removeChild(element);
-      }
-    }
-  }
-});
-**/
-//var aa = new Redirect('/smart/shared/img/1527/smp_150625buyer_25016327_image.jpg');
+})(window, document);

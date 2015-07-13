e6f22f24a30afb6c1630edff68960bf6d8f060e6
syuzyuzyu <syuzyuzyu@gmail.com>
2015-07-13 19:49:03 +0900

    comemmt add

--- timeout-img.js
+++ timeout-img.js
@@ -3,22 +3,29 @@
   //var Redirect = Redirect || {};
   var Redirect = function(ele){
    this._ele = ele;
-   this._src = ele.src;
    this._interval = 1;
    this._replace_img = function(){
      var change_img = document.createElement('img');
      change_img.setAttribute('data-original', ele.src);
      change_img.setAttribute('height', ele.clientHeight);
      change_img.setAttribute('width', ele.clientWidth);
-     return change_img;
+     ele.parentNode.insertBefore(change_img,ele);
+     ele.parentNode.removeChild(ele);
+     //return change_img;
    };
-   this._ele.onload = function () {
-            //TODO event cancel
+    var aa = function(){
+      console.log('startttt');
+      console.log(ele._interval);
     };
+    this._ele.onload = function () {
+        console.log('starrrrrr');
+        console.log(aa);
+        aa();
+     };
+
    this._ele.onabort = this._ele.onerror = function() {
-     var node = this._ele.parentNode;
-     node.insertBefore(replace_tag(),this._ele);
-     node._ele.parentNode.removeChild(this._ele);
+
+      if(typeof _replace_img == "function") ele._replace_img();
     };
   }
   Redirect.prototype = Object.create(Object.prototype, {
@@ -28,32 +35,21 @@
     timeout: {
       value: function() {
         console.log(this._ele.clientHeight);
-        var obj = this._ele, src = this._src, replace_tag = this._replace_img;
-        console.log(src);
+        var obj = this._ele, replace_tag = this._replace_img, parentNode = this._ele.parentNode;
         window.setTimeout(function() {
             console.log(obj.parentNode);
-            if(obj.parentNode !== undefined){obj.src = '';}
+            if(parentNode !== undefined){obj.src = '';}
             //obj.parentNode.removeChild(obj);// || obj.src = '';
                         //TODO:imgタグのsrc属性を抜いて配置し、alt属性をだす３秒後にsrc属性をセットしレジーロードを実施
-            obj.parentNode.insertBefore(replace_tag(),obj);
-            obj.parentNode.removeChild(obj);
+                        replace_tag();
+            //parentNode.insertBefore(replace_tag(),obj);
+            //parentNode.removeChild(obj);
             if(window.stop !== undefined) {
                  window.stop();//to do?
             }
         }, this._interval);
       }
-    }/*,
-  callback: {
-      value: function(){
-        console.log('startdayooooooo');
-        var change_img = document.createElement('img');
-        change_img.setAttribute('data-original', src);
-        change_img.setAttribute('height', this._ele.clientHeight);
-        change_img.setAttribute('width', this._ele.clientWidth);
-        this._ele.parentNode.insertBefore(change_img,this._ele);
-        this._ele.parentNode.removeChild(obj);
-      }*/
-
+    }
   });
   var img_tag = document.getElementsByTagName('img');
   for (var i=0; i<img_tag.length; i++){

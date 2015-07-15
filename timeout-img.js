b1351ca1283e7db8e6e78cf9e3b8c41477de4afa
unknown <suzuki-kenji@NOX375.coheydomain.oisix.co.jp>
2015-07-15 20:17:07 +0900

    comment change

--- timeout-img.js
+++ timeout-img.js
@@ -1,22 +1,34 @@
-(function(window, document){
+(function($, window, document){
   'use strict';
+  var setting = {
+    lazy_load : 'lazy-load',
+    interval : 1,
+    class_name : 'timeout_img',
+    delay_img : -200 /*If the Image is timeout, loding image is lazyload that is delay*/
+  };
   //var Redirect = Redirect || {};
-  var Redirect = function(ele){
+  var Redirect = function(ele, options){
    this._ele = ele;
    this._interval = 1;
+   this._lazy_class = 'lazy-load';
+   this._setting = {
+     interval : 1
+   }
+
    this._replace_img = function(){
      var change_img = document.createElement('img');
-     change_img.setAttribute('data-original', ele.src);
+     change_img.setAttribute('data-original', ele.getAttribute('src'));
      change_img.setAttribute('height', ele.clientHeight);
      change_img.setAttribute('width', ele.clientWidth);
-     change_img.setAttribute('class','lazy-load');
+     //change_img.setAttribute('class','lazy-load');
      change_img.setAttribute('src', '');
      ele.parentNode.insertBefore(change_img,ele);
      ele.parentNode.removeChild(ele);
+     return change_img;
    };
    this._ele.onabort = this._ele.onerror = function() {
      console.log('error occerd');
-     this.setAttribute('data-original', ele.src);
+     this.setAttribute('data-original', ele.getAttribute('src'));
      this.setAttribute('height', ele.clientHeight);
      this.setAttribute('width', ele.clientWidth);
      this.src = '';
@@ -31,23 +43,26 @@
         console.log(this._ele.clientHeight);
         var obj = this._ele, replace_tag = this._replace_img, parentNode = this._ele.parentNode;
         window.setTimeout(function() {
-            console.log(obj.parentNode);
-            if(parentNode !== undefined){obj.src = '';}
+            console.log($.fn.lazyload);
+            //if(parentNode !== undefined){obj.src = '';}
             //obj.parentNode.removeChild(obj);// || obj.src = '';
-                        //TODO:imgタグのsrc属性を抜いて配置し、alt属性をだす３秒後にsrc属性をセットしレジーロードを実施
-              replace_tag();
+              var new_img = replace_tag();
               obj.onabort = obj.onerror=function(){};
+              if($.fn.lazyload !== undefined)
+                    $(new_img).lazyload({threshold : setting.delay_img});
             if(window.stop !== undefined) {
                  window.stop();//to do?
             }
-        }, this._interval);
+        }, setting.interval);
       }
     }
   });
   var img_tag = document.getElementsByTagName('img');
   for (var i=0; i<img_tag.length; i++){
-    if(img_tag[i].className == 'timeout_img'){
+    if(img_tag[i].className == setting.class_name){
       new Redirect(img_tag[i]).timeout();
     }
   }
-})(window, document);
+  //if($.fn.lazyload !== undefined)
+      //  $('.lazy-load').lazyload({threshold : -200});
+})(jQuery, window, document);
